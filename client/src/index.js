e6e3ca0987e096984a66d8101200b6695e3ee51a
DSAghicha <dsaghicha211@gmail.com>
2022-02-06 13:33:51 +0530

    Configured 404 Page

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { Home } from './components'
+import { Home, PageNotFound } from './components'
 
 ReactDOM.render(
     <Router>
         <Routes>
             <Route exact path='/' element={ <Home/> }/>
+            <Route path='*' element={ <PageNotFound/> }/>
         </Routes>
     </Router>,
     document.getElementById('root')

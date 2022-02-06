7e9f9cc1d6e1c2d6e3f10e7e3aaf9f2a2a15d7ac
DSAghicha <dsaghicha211@gmail.com>
2022-02-06 14:21:38 +0530

    Image Assest

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,3 +1,6 @@
 export {  ReactComponent as GirlAppearingExam } from './girl_appearing_exam.svg'
 export { ReactComponent as Logo } from './logo.svg'
 export { ReactComponent as PageNotFoundSVG } from './404.svg'
+
+// Icons
+export { ReactComponent as FirstPage } from './icons/first_page.svg'

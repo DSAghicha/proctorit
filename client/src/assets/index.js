9fc0dfbf84dbad0a62af8ed38474bbae9e39b9b2
DSAghicha <dsaghicha211@gmail.com>
2022-02-06 14:20:36 +0530

    fix: File Name Contradict

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,3 +1,3 @@
 export {  ReactComponent as GirlAppearingExam } from './girl_appearing_exam.svg'
 export { ReactComponent as Logo } from './logo.svg'
-export { ReactComponent as PageNotFound } from './404.svg'
+export { ReactComponent as PageNotFoundSVG } from './404.svg'

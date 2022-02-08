f740cfd04faa6aab97eddf218e260a7f0f3a21f8
DSAghicha <dsaghicha211@gmail.com>
2022-02-08 19:57:31 +0530

    Export Route for Login Component

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,2 +1,3 @@
 export { default as Home } from './home/Home'
+export { default as LoginCandidate } from './auth/candidate_login/LoginCandidate'
 export { default as PageNotFound } from './404/PageNotFound'

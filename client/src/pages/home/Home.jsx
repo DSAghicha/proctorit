22887780db9b14d8a15bc5a04d1c663e550d31de
DSAghicha <dsaghicha211@gmail.com>
2022-02-08 13:27:24 +0530

    Home Page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,25 +1,34 @@
 import './Home.scss'
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { GirlAppearingExam } from '../../assets'
+import { Navbar } from '../../components'
 
 const Home = () => {
     return(
-        <div className='home'>
-            <div className="info">
-                <h1>Conduct credible online proctored tests, anywhere, anytime</h1>
-                <p>Key Features:</p>
-                <ul>
-                    <li>Windows Application for Admin / Invigilator to create and monitor tests.</li>
-                    <li>Multi-device support for candidates.</li>
-                    <li>Fully automated proctoring for candidates.</li>
-                    <li>On-demand live proctoring.</li>
-                </ul>
-                <div className="cta-cr">
-                    
+        <>
+            <Navbar/>
+            <div className='home'>
+                <div className="info">
+                    <h1>Conduct credible online proctored tests, anywhere, anytime</h1>
+                    <p>Key Features:</p>
+                    <ul>
+                        <li>Windows Application for Admin / Invigilator to create and monitor tests.</li>
+                        <li>Multi-device support for candidates.</li>
+                        <li>Fully automated proctoring for candidates.</li>
+                        <li>On-demand live proctoring.</li>
+                    </ul>
+                    <div className="cta-cr">
+                        <Link to='/demo'>Take a demo test</Link>
+                        <Link to='/institute-signup'>Register Institute</Link>
+                    </div>
+                </div>
+                <GirlAppearingExam/>
+                <div className="scroll-icon">
+                    <div className="circle"></div>
                 </div>
             </div>
-            <GirlAppearingExam/>
-        </div>
+        </>
     )
 }
 

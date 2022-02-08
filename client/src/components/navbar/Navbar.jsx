f503382773033a88c821459224053aecb55daade
DSAghicha <dsaghicha211@gmail.com>
2022-02-08 13:23:48 +0530

    Navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,31 +1,47 @@
 import './Navbar.scss'
-import React from 'react'
+
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { Logo } from '../../assets'
+import { DropDownSVG, Logo } from '../../assets'
 
 const Navbar = () => {
+    const [dropdownMenu, openDropdownMenu]  = useState(false)
+
+    const handleSubmenu = () => openDropdownMenu(!dropdownMenu)
     return(
         <header>
             <div className="logo-cr">
                 <Logo/>
             </div>
             <nav>
-                <ul>
-                    <li>
+                <ul className="nav__menu">
+                    <li className="nav__menu__item">
                         <Link to='/'>HOME</Link>
                     </li>
-                    <li>
-                    <Link to='/'>SERVICES & SOLUTIONS</Link>
-
+                    <li className="nav__menu__item">
+                        <span className={ dropdownMenu ? 'nav__submenu__title svgFlip' : 'nav__submenu__title'} onClick={handleSubmenu}>
+                            <>SERVICES &amp; SOLUTIONS</>
+                            <DropDownSVG/>
+                        </span>
+                            { dropdownMenu && 
+                                <ul className='nav__submenu'>
+                                    <li className='nav__submenu__item'>
+                                        <Link to='/online-proctoring'>ONLINE PROCTORING</Link>
+                                    </li>
+                                    <li className='nav__submenu__item'>
+                                        <Link to='/live-proctoring'>LIVE PROCTORING</Link>
+                                    </li>
+                                </ul>
+                            }
                     </li>
-                    <li>
-                        <Link to='/'>ABOUT US</Link>
+                    <li className="nav__menu__item">
+                        <Link to='/about-us'>ABOUT US</Link>
                     </li>
-                    <li>
-                        <Link to='/'>CONTACT US</Link>
+                    <li className="nav__menu__item">
+                        <Link to='/contact-us'>CONTACT US</Link>
                     </li>
-                    <li>
-                        <Link to='/'>CANDIDATE LOGIN</Link>
+                    <li className="nav__menu__item">
+                        <Link to='/auth/candidate/login'>CANDIDATE LOGIN</Link>
                     </li>
                 </ul>
             </nav>

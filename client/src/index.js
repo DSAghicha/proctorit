083967012a8f9431017a8b3a49c30031dc406183
DSAghicha <dsaghicha211@gmail.com>
2022-02-08 19:58:03 +0530

    Route Setup for Candidate Login Component

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { Home, PageNotFound } from './pages'
+import { Home, LoginCandidate, PageNotFound } from './pages'
 
 ReactDOM.render(
     <Router>
         <Routes>
             <Route exact path='/' element={ <Home/> }/>
+            <Route exact path='/auth/candidate/login' element={ <LoginCandidate/> }/>
             <Route path='*' element={ <PageNotFound/> }/>
         </Routes>
     </Router>,

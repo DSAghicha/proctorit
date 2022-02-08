7070e8630f111bf93ad94b38cb66b5a5856af10c
DSAghicha <dsaghicha211@gmail.com>
2022-02-08 20:38:26 +0530

    Route Config for Institute Registration

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { Home, LoginCandidate, PageNotFound } from './pages'
+import { Home, LoginCandidate, PageNotFound, RegisterInstitute } from './pages'
 
 ReactDOM.render(
     <Router>
         <Routes>
             <Route exact path='/' element={ <Home/> }/>
             <Route exact path='/auth/candidate/login' element={ <LoginCandidate/> }/>
+            <Route exact path='/auth/institute-signup' element={ <RegisterInstitute/> }/>
             <Route path='*' element={ <PageNotFound/> }/>
         </Routes>
     </Router>,
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from './home/Home'
 export { default as LoginCandidate } from './auth/candidate_login/LoginCandidate'
+export { default as RegisterInstitute } from './auth/institute_register/RegisterInstitute'
 export { default as PageNotFound } from './404/PageNotFound'
